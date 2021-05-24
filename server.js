8485b7b8cd0c307c4e1f5b484bafc20d56640207
Michael Gant <your_email_here>
2021-05-24 16:15:37 -0400

    bcrypt hashed

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const { syncAndSeed } =  require('./db');
 const app = require('./app');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt')
 
 const SECRET_KEY = 'lebronflops';
 
@@ -8,6 +9,12 @@ const token = jwt.sign({ username: 'user1234' }, SECRET_KEY);
 
 const verifyGood = jwt.verify(token, SECRET_KEY);
 
+const hashPassword = async (password) => {
+  let hashed = await bcrypt.hash(password, 5);
+  console.log(hashed);
+  const correct = await bcrypt.compare(password, hashed)
+  console.log(correct)
+}
 const init = async() => {
   await syncAndSeed();
   const port = process.env.PORT || 3000;
@@ -15,3 +22,4 @@ const init = async() => {
 };
 
 init();
+// hashPassword('password');
