0b0e2b89423a4a51593686d81cc63c15d8af4be2
Hernan Berisso <hberisso1@gmail.com>
2021-05-24 13:00:51 -0400

    created token generator and verifier

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const { syncAndSeed } =  require('./db');
 const app = require('./app');
+const jwt = require('jsonwebtoken');
+
+const SECRET_KEY = 'lebronflops';
+
+const token = jwt.sign({ username: 'user1234' }, SECRET_KEY);
+
+const verifyGood = jwt.verify(token, SECRET_KEY);
 
 const init = async() => {
   await syncAndSeed();
